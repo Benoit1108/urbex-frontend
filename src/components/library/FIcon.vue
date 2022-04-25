d384b088e2a3282d8cc86e1c546047c0b385e185
Benoit <benoit.2001@hotmail.fr>
2022-04-25 09:32:33 +0200

    feat: components feather icon

---
+++ src/components/library/FIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-html="iconSvg"></div>
+</template>
+
+<script>
+import Feather from 'feather-icons';
+export default {
+  inheritAttrs: false,
+  props: {
+    size : {
+      type: Number,
+      required: false,
+      default: 24
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconSvg() {
+      return Feather.icons[this.name].toSvg({ class: this.$attrs.class, width: this.size, height: this.size });
+    },
+  },
+};
+</script>
